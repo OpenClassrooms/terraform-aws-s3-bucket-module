c2260baea918fab53119730d0de528758e9e9e5f
Jeremy GOVI <jeremygovi@gmail.com>
2022-05-12 11:42:01 +0200

    add website_endpoint output

--- output.tf
+++ output.tf
@@ -3,3 +3,7 @@ output "bucket_regional_domain_name" {
   value       = aws_s3_bucket.s3_bucket.bucket_regional_domain_name
 }
 
+output "website_endpoint" {
+  description = "The bucket website_endpoint"
+  value       = aws_s3_bucket.s3_bucket.website_endpoint
+}
