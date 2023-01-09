0177ede08b570576218ef3690d4799a4cb5be35a
jeremygovi <jeremygovi@gmail.com>
2023-01-09 11:06:39 +0100

    add some essential outputs

--- output.tf
+++ output.tf
@@ -1,3 +1,11 @@
+output "name" {
+  description = "The bucket name"
+  value       = aws_s3_bucket.s3_bucket.name
+}
+output "arn" {
+  description = "The bucket ARN"
+  value       = aws_s3_bucket.s3_bucket.arn
+}
 output "bucket_regional_domain_name" {
   description = "The bucket_regional_domain_name"
   value       = aws_s3_bucket.s3_bucket.bucket_regional_domain_name
